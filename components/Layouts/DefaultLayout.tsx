05e9c337aa5098b5bcc78748b2ee41b2ae0e6f6b
evildeedz <sahilverma1311@protonmail.com>
2024-03-12 16:36:43 +0530

    Header  footer updated

--- components/Layouts/DefaultLayout.tsx
+++ components/Layouts/DefaultLayout.tsx
@@ -117,13 +117,12 @@ const DefaultLayout = ({ children, showHeader = true, showFooter = true }: Props
                 )}
 
                 <div className="flex min-h-screen flex-col bg-white bg-gradient-to-r from-[#FCF1F4] to-[#EDFBF9] font-montserrat text-base font-normal text-gray antialiased dark:bg-[#101926] dark:from-transparent dark:to-transparent">
-                    {showHeader && <Header />}
-                    {path === '/pages/pagie' ? <Header /> : null}
-                 
-                    
+                    {/* {(path !== '/pagie' || path !== '/page') ? <Header /> : <></>} */}
+                    {path !== '/pagie' ? <Header /> : <></>}
+
                     <div className="-mt-[82px] flex-grow overflow-x-hidden lg:-mt-[106px]">{children}</div>
-                    
-                    {showFooter && <Footer />}
+
+                    {path !== '/pagie' ? <Footer /> : <></>}
                 </div>
 
                 {/* Return to Top  */}

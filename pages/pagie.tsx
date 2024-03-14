29b7d4744fc4a944a701c2a8a2081d42771db0ac
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-03-15 00:39:34 +0530

    Update pagie.tsx

--- pages/pagie.tsx
+++ pages/pagie.tsx
@@ -94,8 +94,8 @@ const Page = () => {
                         <h2>Select the company</h2>
                     </div>
                     <div className="image-row">
-                        <img src="/assets/images/large_logo.png" alt="Image 1" className="single-image" />
-                        <img src="https://media.discordapp.net/attachments/1193183717548638301/1217887367516655696/image-removebg-preview_10.png?ex=6605a8ff&is=65f333ff&hm=4bc0e797de6da35960295d7538d81b058b1d0386a4ae8a5a29d69fdf5d34d673&=&format=webp&quality=lossless&width=613&height=613" alt="Image 2" className="single-image" />
+                        <img src="/assets/images/l.png" alt="Image 1" className="single-image" />
+                        <img src="/assets/images/log.png" alt="Image 2" className="single-image" />
                         <img src="https://media.discordapp.net/attachments/1193183717548638301/1215705946140053596/image-removebg-preview_9.png?ex=65fdb964&is=65eb4464&hm=082adccdaa24f5be6d3759b6795a7a72ec9372d4c81a7d0599a8a195272b29af&=&format=webp&quality=lossless&width=400&height=400" alt="Image 3" className="single-image" />
                         <img src="https://t3.ftcdn.net/jpg/01/29/69/84/360_F_129698446_e2kKKd1MgIDJJzxfO1sE0jeUSn88SzKx.jpg" alt="Image 4" className="single-image" />
                     </div>
